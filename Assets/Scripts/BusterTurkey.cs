9f1a61a6ea3d72fadbab653e68107c229d3fead5
Charlie Barto <bartoc@umich.edu>
2014-10-13 16:07:07 -0400

    Added enemy AI

---
+++ Assets/Scripts/BusterTurkey.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class BusterTurkey : RangedEnemy
+{
+    private int lastTurret = 0;
+    public List<GameObject> Turrets;
+    public GameObject projectile;
+    public float BulletSpeed = 1000.0f;
+    protected override void HandleAttack()
+    {
+        lastTurret = (lastTurret + 1)%Turrets.Count;
+        var turret = Turrets[lastTurret].transform;
+        var bullet = (GameObject)Instantiate(projectile, turret.position, turret.rotation);
+        bullet.rigidbody.AddForce(transform.forward * BulletSpeed);
+    }
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+}
