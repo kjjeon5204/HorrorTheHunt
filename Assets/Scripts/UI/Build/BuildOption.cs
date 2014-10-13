5ab61bb38d4c9723829992f42bdb221c74f872b6
kjjeon5204 <bboybyung@gmail.com>
2014-10-13 16:26:05 -0400

    unfucked kevin's shit again

---
+++ Assets/Scripts/UI/Build/BuildOption.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildOption : BaseButton {
+    public GameObject buildObject;
+
+    public override GameObject get_button_object()
+    {
+        return buildObject;
+    }
+}
