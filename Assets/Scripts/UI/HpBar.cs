ee205033c86d00fb48c37a3a6e70660dd580082f
kjjeon5204 <bboybyung@gmail.com>
2014-10-14 02:42:03 -0400

    update

---
+++ Assets/Scripts/UI/HpBar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HpBar : MonoBehaviour {
+    public CharController mainCharacter;
+    public GameObject bar;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 tempHolder = bar.transform.localScale;
+        if (mainCharacter.maxHP != 0)
+            tempHolder.x = mainCharacter.playerHP / mainCharacter.maxHP;
+        if (mainCharacter.maxHP < 0)
+        {
+            tempHolder.x = 0;
+        }
+        bar.transform.localScale = tempHolder;
+	}
+}
