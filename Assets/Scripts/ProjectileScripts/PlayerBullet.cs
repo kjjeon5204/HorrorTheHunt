91c8658a1f247d1339a35ff56422dee6e4d7fce0
Charlie Barto <bartoc@umich.edu>
2014-10-14 11:36:11 -0400

    added player bullet

---
+++ Assets/Scripts/ProjectileScripts/PlayerBullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBullet : MonoBehaviour {
+    public int Damage = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
